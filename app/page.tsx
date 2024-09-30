d8c87eeef400a04bed5bece3c4130df5ce440b16
Ibe Noriaki <ibenoriaki@gmail.com>
2024-10-01 02:06:39 +0900

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { ShareTicketButton } from './components/ShareTicketButton';
 import { ConnectToReceiveButton } from './components/ConnectToReceiveButton';
 import DrinkTicketApp from './components/DrinkTicketApp';
+import { SpecificTradeInfoPopup } from './components/SpecificTradeInfoPopup';
 
 export default function Home() {
   return (
@@ -32,52 +33,8 @@ export default function Home() {
           <ConnectToReceiveButton />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center text-gray-400">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4 hover:text-white transition-colors"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="row-start-3 flex justify-center">
+        <SpecificTradeInfoPopup />
       </footer>
     </div>
   );
